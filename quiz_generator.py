fa8cb606079370261af7d8a9b6685d647ed9cf7e
CapedHero <mc.wrzesniewski@gmail.com>
2017-09-04 10:17:57 +0200

    quiz_generator.py - Move code to function.

--- quiz_generator.py
+++ quiz_generator.py
@@ -6,6 +6,60 @@
 import os
 import random
 
+
+def quiz_generator(data_set, quizzes_num=14, questions_num=50, options_num=4):
+    lead_zeros = len(str(quizzes_num))
+    if not os.path.exists('tests'): os.makedirs('tests')
+    if not os.path.exists('answers'): os.makedirs('answers')
+
+    for quiz_no in range(1, quizzes_num + 1):
+        # Create quiz and answer key files.
+        quiz_no_padded = str(quiz_no).zfill(lead_zeros)
+        quiz_name = 'capitals_quiz_' + quiz_no_padded
+
+        path_quiz = os.path.join('tests', (quiz_name + '.txt'))
+        quiz_file = open(path_quiz, 'w')
+
+        path_answers = os.path.join('answers', (quiz_name + '_answers.txt'))
+        answers_file = open(path_answers, 'w')
+
+        # Write the header.
+        quiz_file.write('''
+    Name: 
+    Date:
+
+
+    {} (Form {})
+               
+                                  
+    '''.format('STATE CAPITALS QUIZ', quiz_no_padded))
+
+        # Write questions and answers.
+        questions = list(data_set.keys())
+        random.shuffle(questions)
+
+        for question_no in range(1, questions_num + 1):
+            question = questions[question_no - 1]
+            correct_answer = data_set[question]
+
+            wrong_answers = list(data_set.values())
+            wrong_answers.remove(correct_answer)
+            wrong_answers = random.sample(wrong_answers, options_num - 1)
+
+            all_answers = wrong_answers + [correct_answer]
+            random.shuffle(all_answers)
+
+            quiz_file.write('%d. What is the capital of %s?\n' % (question_no, question))
+            for i in range(options_num):
+                quiz_file.write('\t%s. %s\n' % ('ABCD'[i], all_answers[i]))
+            quiz_file.write('\n')
+
+            answers_file.write('%s. %s\n' % (question_no, correct_answer))
+
+        quiz_file.close()
+        answers_file.close()
+
+
 # Quiz test data. Keys are states and values are their capitals.
 usa_capitals = {
     'Alabama': 'Montgomery',
@@ -60,57 +114,4 @@ usa_capitals = {
     'Wyoming': 'Cheyenne'
 }
 
-questions_and_answers = usa_capitals
-quizzes_num = 14
-questions_num = 50
-options_num = 4
-lead_zeros = len(str(quizzes_num))
-if not os.path.exists('tests'): os.makedirs('tests')
-if not os.path.exists('answers'): os.makedirs('answers')
-
-for quiz_no in range(1, quizzes_num + 1):
-    # Create quiz and answer key files.
-    quiz_no_padded = str(quiz_no).zfill(lead_zeros)
-    quiz_name = 'capitals_quiz_' + quiz_no_padded
-
-    path_quiz = os.path.join('tests', (quiz_name + '.txt'))
-    quiz_file = open(path_quiz, 'w')
-
-    path_answers = os.path.join('answers', (quiz_name + '_answers.txt'))
-    answers_file = open(path_answers, 'w')
-
-    # Write the header.
-    quiz_file.write('''
-Name: 
-Date:
-
-
-{} (Form {})
-           
-                              
-'''.format('STATE CAPITALS QUIZ', quiz_no_padded))
-
-    # Write questions and answers.
-    questions = list(questions_and_answers.keys())
-    random.shuffle(questions)
-
-    for question_no in range(1, questions_num + 1):
-        question = questions[question_no - 1]
-        correct_answer = questions_and_answers[question]
-
-        wrong_answers = list(questions_and_answers.values())
-        wrong_answers.remove(correct_answer)
-        wrong_answers = random.sample(wrong_answers, options_num - 1)
-
-        all_answers = wrong_answers + [correct_answer]
-        random.shuffle(all_answers)
-
-        quiz_file.write('%d. What is the capital of %s?\n' % (question_no, question))
-        for i in range(options_num):
-            quiz_file.write('\t%s. %s\n' % ('ABCD'[i], all_answers[i]))
-        quiz_file.write('\n')
-
-        answers_file.write('%s. %s\n' % (question_no, correct_answer))
-
-    quiz_file.close()
-    answers_file.close()
+quiz_generator(usa_capitals)
